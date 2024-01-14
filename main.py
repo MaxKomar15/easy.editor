3b572165cc630ffa8e27eb1e79cb3c13a2a085a0
MaxKomar15 <143327983+MaxKomar15@users.noreply.github.com>
2024-01-14 15:26:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap
 from ui import Ui_MainWindow
 import os
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 
 class EasyEditor(QMainWindow):
     def   __init__(self):
@@ -13,6 +13,7 @@ class EasyEditor(QMainWindow):
         self.ui.setupUi(self)
         self.workdir = None
         self.filename = None
+        self.last_image = None        
         self.image = None
         self.save_folder = "Edited/"
         self.image_name = None
@@ -22,7 +23,12 @@ class EasyEditor(QMainWindow):
         self.ui.folder_btn.clicked.connect(self.choose_folder)
         # self.ui.action.triggered.connect(self.choose_folder)
         self.ui.image_list.currentRowChanged.connect(self.show_chosen_image)
-        self.ui.bw_btn.clicked.connect
+        self.ui.BW_btn.clicked.connect(self.do_black)
+        self.ui.left_btn.clicked.connect(self.do_left)
+        self.ui.right_btn.clicked.connect(self.do_right)
+        self.ui.mirror_btn.clicked.connect(self.do_mirror)
+        self.ui.back_btn.clicked.connect(self.do_back)
+        
 
 
     def filter(self, filenames):
@@ -49,13 +55,13 @@ class EasyEditor(QMainWindow):
     def load_image(self, filename):
         self.image_name = filename
         self.filename = os.path.join(self.workdir, filename)
-        self.imgae  = Image.open(self.filename)
+        self.image  = Image.open(self.filename)
        
 
 
 
     def show_image(self):
-        self.ui.image_label.hide
+        self.ui.image_label.hide()
         h =self.ui.image_label.height()
         w =self.ui.image_label.width()
         
@@ -64,29 +70,55 @@ class EasyEditor(QMainWindow):
         self.ui.image_label.setPixmap(pm_image)
         self.ui.image_label.show()
 
-
     def show_chosen_image(self):
         if self.ui.image_list.currentRow() >= 0:
             filename = self.ui.image_list.currentItem().text()
             self.load_image(filename)
             self.show_image()
+
     def save_image(self):
         path = os.path.join(self.workdir, self.save_folder)
         if not(os.path.exists(path) or os.path.isdir(path)):
             os.mkdir(path)
         self.filename = os.path.join(path, self.image_name)
+
         self.image.save(self.filename)
-
-
-
+        
 
     def do_black(self):
-        self.image = self.image.convert("L")
-        self.shjw_image()
-        self.show_image()
+        if self.image:
+            self.last_image = self.image
+            self.image = self.image.convert("L")
+            self.save_image()
+            self.show_image()
 
+    def do_left(self):
+        if self.image:
+            self.last_image = self.image
+            self.image = self.image.transpose(Image.ROTATE_90)
+            self.save_image()
+            self.show_image()
 
+    def do_right(self):
+        if self.image:
+            self.last_image = self.image
+            self.image = self.image.transpose(Image.ROTATE_270)
+            self.save_image()
+            self.show_image()
 
+    def do_mirror(self):
+        if self.image:
+            self.last_image = self.image
+            self.image =ImageOps.mirror(self.image)
+            self.save_image()
+            self.show_image()
+
+    def do_back(self):
+
+        if self.image and self.last_image:
+            self.image = self.last_image
+            self.save_image()
+            self.show_image()
 
 
 app = QApplication([])
